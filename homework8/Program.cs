41de8eb022096ac6c91d8c802a4f0ddb402be35c
Siro <89258189+Siromu@users.noreply.github.com>
2022-10-28 18:18:07 +0600

    Add files via upload

---
+++ homework8/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Type typeDate = typeof(DateTime); //получение типа DateTime в переменной typeDate
+
+foreach (var prop in typeDate.GetProperties()) //пока prop в пределах свойств DateTime
+                                               //.GetProperties() возвращает ВСЕ свойства структуры 
+    Console.WriteLine(prop.Name); //выводим названия свойств
+
+Console.ReadKey(); //для красоты
